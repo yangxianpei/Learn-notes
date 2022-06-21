cf35b6a57b0d577dcbe9097ead847b62f706a008
yangxianpei <1013128101@qq.com>
2022-06-21 14:20:04 +0800

    2022/6/21 react常见hook2

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run docs:build
 echo '准备'
 git init
 git add .
-git commit -m "2022/6/21 react常见hook"
+git commit -m "2022/6/21 react常见hook2"
 echo '发布主分支'
 # 如果发布到 master
 git push -f git@github.com:yangxianpei/Learn-notes.git master
@@ -18,7 +18,7 @@ echo '发布子分支'
 cd docs/.vuepress/build
 git init
 git add -A
-git commit -m '2022/6/21 react常见hook'
+git commit -m '2022/6/21 react常见hook2'
 # 如果发布到 gh-pages
 echo '开始子分支上传'
 git push -f git@github.com:yangxianpei/Learn-notes.git master:gh-pages
