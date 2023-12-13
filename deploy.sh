4b05af92ebed0e432dbe86cdeb187cb501d22c8d
杨贤培 <46846142+yangxianpei@users.noreply.github.com>
2023-12-13 21:23:51 +0800

    2023/12/13 less笔记2

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ npm run docs:build
 echo '准备'
 git init
 git add .
-git commit -m "2023/12/13 less笔记"
+git commit -m "2023/12/13 less笔记2"
 echo '发布主分支'
 # 如果发布到 master
 git push -f git@github.com:yangxianpei/Learn-notes.git master
@@ -18,7 +18,7 @@ echo '发布子分支'
 cd docs/.vuepress/build
 git init
 git add -A
-git commit -m '2023/12/13 less笔记'
+git commit -m '2023/12/13 less笔记2'
 # 如果发布到 gh-pages
 echo '开始子分支上传'
 git push -f git@github.com:yangxianpei/Learn-notes.git master:gh-pages
